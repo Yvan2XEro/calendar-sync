c8cccc354561b1a4e9d2fac254b610a79eed045c
Yvan2xEro <yvankanaj@gmail.com>
2025-09-19 10:20:20 +0100

    feat: admin feat

--- apps/server/src/lib/auth-client.ts
+++ apps/server/src/lib/auth-client.ts
@@ -1,14 +1,16 @@
 import {
-	inferOrgAdditionalFields,
-	organizationClient,
+  inferOrgAdditionalFields,
+  organizationClient,
+  adminClient,
 } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
-	plugins: [
-		organizationClient({
-			schema: inferOrgAdditionalFields<typeof auth>(),
-		}),
-	],
+  plugins: [
+    organizationClient({
+      schema: inferOrgAdditionalFields<typeof auth>(),
+    }),
+    adminClient(),
+  ],
 });
