11c0b48dd12dff06fca2a2d196281b80d9a8283a
Yvan Jules Kana <63490400+Yvan2XEro@users.noreply.github.com>
2025-09-25 08:42:04 +0100

    refactor admin events list view components

---
+++ apps/server/src/components/admin/events/EventActionsMenu.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import type { StatusAction } from "./status-actions";
+
+type EventActionsMenuProps = {
+	statusActions: StatusAction[];
+	onUpdateStatus: (status: StatusAction["status"]) => void;
+	onEdit: () => void;
+	onView: () => void;
+	disabled?: boolean;
+};
+
+export function EventActionsMenu({
+	statusActions,
+	onUpdateStatus,
+	onEdit,
+	onView,
+	disabled = false,
+}: EventActionsMenuProps) {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="ghost" size="icon" disabled={disabled}>
+					<MoreHorizontal className="size-4" />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent align="end" className="w-48">
+				<DropdownMenuLabel>Moderation</DropdownMenuLabel>
+				{statusActions.map((action) => (
+					<DropdownMenuItem
+						key={action.status}
+						onClick={() => onUpdateStatus(action.status)}
+					>
+						<action.icon className="mr-2 size-4" />
+						{action.label}
+					</DropdownMenuItem>
+				))}
+				<DropdownMenuSeparator />
+				<DropdownMenuItem onClick={onEdit}>Edit event</DropdownMenuItem>
+				<DropdownMenuItem onClick={onView}>View details</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
