ff0fec2ebfddd0ea1e837f3f12d777544830d17c
Yvan2xEro <yvankanaj@gmail.com>
2025-09-26 07:37:39 +0100

    feat: user dropdown menu

--- apps/server/src/components/UserAvatar.tsx
+++ apps/server/src/components/UserAvatar.tsx
@@ -1,10 +1,38 @@
+"use client"
+import { authClient } from "@/lib/auth-client";
 import { UserAvatar as UIUserAvatar } from "@daveyplate/better-auth-ui";
+import { LogOut, User } from "lucide-react";
 import Link from "next/link";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export const UserAvatar = () => {
-	return (
-		<Link href="/account/settings">
-			<UIUserAvatar />
-		</Link>
-	);
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        {/* <Link href="/account/settings"> */}
+        <UIUserAvatar />
+        {/* </Link> */}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+			<User />
+            <Link href="/account/settings">Manage Account</Link>
+          </DropdownMenuItem>
+		  <DropdownMenuSeparator />
+          <DropdownMenuItem onSelect={()=>authClient.signOut()}>
+			<LogOut />
+            <span>Manage Account</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
